912fe28b9fff243183e58931ec619d10d226f6fb
CollinRoess <collinoeight@gmail.com>
2018-07-19 23:14:57 -0400

    Starting genetic algo.

---
+++ genetic.py
@@ -0,0 +1,5 @@
+import pyges
+
+pyges.get_file(input('Enter IGES filename: '))
+
+gene_pool=pyges.get_points()
--- pyges.py
+++ pyges.py
@@ -5,19 +5,13 @@ Maybe it was. Anyway pipe down. I'll fix it.
 import os
 
 def get_file(file_name):
-    global file
     file = open(os.path.abspath(file_name))
-
-def print_text(file):
-    for line in file:
-        print(line)
-
-def get_parametric_data(file):
     global x
     x=[]
     for line in file:
         if line[72]=='P':
             x.append(line[:65])
+
 def get_points():
     y=[]
     pointlist=[]
@@ -46,14 +40,3 @@ def get_points():
             except ValueError:
                 pass
     return(sorted(xyz))
-
-if __name__ == '__main__':
-    print('NOTE: Filenames must be in the same directory as this program')
-    file_name =  input('Please enter a filename, or enter \'(h)elp\': ')
-    if file_name == 'h' or file_name == 'H':
-        get_file('readme.md')
-        print(file.read())
-    else:
-        get_file(file_name)
-        get_parametric_data(file)
-        print(get_points())
